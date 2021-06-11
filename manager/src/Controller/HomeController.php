ab327aadb56a8f17b4d241efd1afdbc20d706dc3
artem.koval <artem.koval@codeit.pro>
2021-06-11 11:39:05 +0300

    fix pgsql docker settings

--- manager/src/Controller/HomeController.php
+++ manager/src/Controller/HomeController.php
@@ -5,17 +5,16 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
     /**
-     * @Route('/', name='home')
+     * @Route("/", name="home")
      * @return Response
      */
-    public function index(Request $request): Response
+    public function index(): Response
     {
         return $this->render('app/home.html.twig');
         // return new Response('hello');
